575e053c1b504133100d93a1ce5c8b82edd0c06b
Camille Roussel <camille@rousselfamily.com>
2014-12-08 21:54:05 -0500

    final chapter test 3+4 done.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -59,14 +59,18 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
+    /* try chaining range() and reduce() */
+    var sum = _(_.range(1000))
+                .chain()
+                .filter(function (num) {return num % 3 === 0 || num % 5 === 0;})
+                .reduce(function (memo, num) {return memo + num;})
+                .value();
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
-
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
