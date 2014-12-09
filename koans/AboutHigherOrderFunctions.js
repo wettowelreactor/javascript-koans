07a3fc1f188d7ad8372e8064ddc62e82bd406c90
Camille Roussel <camille@rousselfamily.com>
2014-12-08 21:12:50 -0500

    AboutHigherOrderFunctions test 3 done.

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -34,8 +34,8 @@ describe("About Higher Order Functions", function () {
       /* initial */ 0
     );
 
-    expect(reduction).toBe(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(reduction).toBe(6);
+    expect(numbers).toEqual([1, 2, 3]);
   });
 
   it("should use 'forEach' for simple iteration", function () {
