520477279de80ec093fa74c95ad4f3365611540f
Camille Roussel <camille@rousselfamily.com>
2015-01-12 19:47:04 -0500

    2015 update

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -44,8 +44,8 @@ describe("About Objects", function () {
       }
     };
 
-    expect(currentYear).toBe(2014);
-    expect(meglomaniac.calculateAge()).toBe(44);
+    expect(currentYear).toBe(2015);
+    expect(meglomaniac.calculateAge()).toBe(45);
   });
 
   describe("'in' keyword", function () {
